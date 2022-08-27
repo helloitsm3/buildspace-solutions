d571e4116ef6e5baf9dc4d8b070b45b445509b95
Sean <seanlengws@gmail.com>
2022-08-27 09:34:36 +0800

    Added dynamic price for domains

--- cool-domains/scripts/deploy.js
+++ cool-domains/scripts/deploy.js
@@ -4,18 +4,21 @@ const main = async () => {
     await domainContract.deployed();
 
     console.log("Contract deployed to:", domainContract.address);
-
+    const domainName = "banana";
+    
+    let rawPrice = await domainContract.price(domainName);
+    let price = (parseInt(rawPrice, 10) / 10 ** 18).toString();
     // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
-    let txn = await domainContract.register("banana", { value: hre.ethers.utils.parseEther("0.1") });
+    let txn = await domainContract.register(domainName, { value: hre.ethers.utils.parseEther(price) });
     await txn.wait();
-    console.log("Minted domain banana.ninja");
+    console.log(`Minted domain ${domainName}.ninja`);
 
-    txn = await domainContract.setRecord("banana", "Am I a banana or a ninja??");
+    txn = await domainContract.setRecord(domainName, "Am I a banana or a ninja??");
     await txn.wait();
     console.log("Set record for banana.ninja");
 
-    const address = await domainContract.getAddress("banana");
-    console.log("Owner of domain banana:", address);
+    const address = await domainContract.getAddress(domainName);
+    console.log(`Owner of domain ${domainName}:`, address);
 
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
