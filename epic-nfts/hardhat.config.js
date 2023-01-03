f7c90bd48d63ef8fe79c25ed046d9ec04fc9772b
ScorpiusSigma <79987741+ScorpiusSigma@users.noreply.github.com>
2023-01-03 11:15:32 +0800

    Changed network from rinkeby to goerli

--- epic-nfts/hardhat.config.js
+++ epic-nfts/hardhat.config.js
@@ -20,7 +20,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
     solidity: "0.8.4",
     networks: {
-        rinkeby: {
+        goerli: {
             url: process.env.ALCHEMY_API_URL,
             accounts: [process.env.PRIVATE_KEY],
         },
